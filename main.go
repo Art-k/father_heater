7518faa79eb6358a69c95e238b3621a6bc3040ca
kryhin <artem@maxtvmedia.com>
2019-11-02 03:13:32 +0200

    go to port 8080

--- main.go
+++ main.go
@@ -36,7 +36,7 @@ func main() {
 	http.HandleFunc("/set_data", setSensorData)
 
 	fmt.Printf("Starting server for testing HTTP POST...\n")
-	if err := http.ListenAndServe(":5000", nil); err != nil {
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
 
